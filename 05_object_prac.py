6c0e2831b8ae50d1f0a260d14aca35f6b52f4713
ujuj20030909-hub <ujuj20030909@gmail.com>
2026-07-16 01:21:52 +0900

    산술 연산자 실습

---
+++ 05_object_prac.py
@@ -0,0 +1,35 @@
+count = 5
+temp = 34.2
+name = "안녕"
+is_ok = True
+print(count, temp, name, is_ok)
+
+print(type(count))
+print(type(temp))
+print(type(name))
+print(type(is_ok))
+
+print(type(100))  # <class 'int'>
+print(type(100.0))  # <class 'float'>
+print(type("100"))  # <class 'str'>
+
+print(5 + 2)
+print("5" + "2")
+print("20" + "26")
+
+print(3 > 2)  # True
+print(5 == 5)  # True
+print(type(3 > 2))  # <class 'bool'>
+
+count = 2
+print(count, type(count))
+count = 2.0
+print(count, type(count))
+count = "2"
+print(count, type(count))
+
+device_temp = 32.4
+check_count = 5
+device_name = "센서 A"
+is_normal = True
+print(device_temp, check_count, device_name, is_normal)
