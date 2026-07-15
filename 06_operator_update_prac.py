5c7af52088b8cc471d05252d2cce0552d2b77a7b
ujuj20030909-hub <ujuj20030909@gmail.com>
2026-07-16 01:25:49 +0900

    산술 연산자 실습

---
+++ 06_operator_update_prac.py
@@ -0,0 +1,20 @@
+# 실습
+
+a = 6
+b = 2
+print(a + b)
+print(a - b)
+print(a * b)
+print(a / b)
+print(a // b)
+print(a % b)
+print(a**b)
+
+
+a = 30
+b = 60
+c = 20
+print((30 + 60 + 90) / 3)
+side = 6
+print(side ** 2)
+print(3 * 6 * 2)
