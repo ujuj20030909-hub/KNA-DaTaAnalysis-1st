b9d7dcec50b0a4ddf1c427ddc41a075eebfacce2
ujuj20030909-hub <ujuj20030909@gmail.com>
2026-07-16 11:05:05 +0900

    study 연산자 활용

--- 06_operator_update_prac.py
+++ 06_operator_update_prac.py
@@ -16,5 +16,43 @@ b = 60
 c = 20
 print((30 + 60 + 90) / 3)
 side = 6
-print(side ** 2)
+print(side**2)
 print(3 * 6 * 2)
+
+print("하이" == "하이")
+print(7 != 7)
+print(7 > 6)
+print(7 < 6)
+print(11 >= 6)
+print(10 <= 10)
+
+
+print("=======")
+temp = 85
+temp_is = 60 <= temp and temp <= 90
+print(temp_is)
+pressure = 5
+pressure_is = 3 <= pressure and pressure <= 7
+print(pressure_is)
+print(temp_is and pressure_is)
+
+
+stoke = 100
+print(stoke)
+stoke += 50
+print(stoke)
+stoke -= 30
+print(stoke)
+stoke += 5
+print(stoke)
+
+total = 500
+non = 23
+print(non / total * 100)
+run_h = 21
+all_h = 24
+print(run_h / all_h * 100)
+
+minutes = 500
+print(500 // 60)
+print(500 % 60)
