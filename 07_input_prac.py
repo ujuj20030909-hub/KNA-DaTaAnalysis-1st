3ce4728b3e4b032b41c15993f5867cc39b0844c0
ujuj20030909-hub <ujuj20030909@gmail.com>
2026-07-16 14:28:09 +0900

    study: input 실습

---
+++ 07_input_prac.py
@@ -0,0 +1,28 @@
+# 실습
+
+# name = input("이름은: ")
+# print("안녕하세요", name + "님!")
+
+# birth_year = int(input("태어난 해: "))
+# print("내 나이는 ", 2026 - birth_year + 1, "살 입니다.")
+
+# country = input("국가: ")
+# city = input("도시: ")
+# print(country + "의", city + "에서", "거주하시는군요!")
+
+# a = int(input("첫 번째 숫자: "))
+# b = int(input("두 번째 숫자: "))
+# print("두 수의 곱은: ", a * b)
+# print("두 수의 차는: ", b - a)
+
+# 80 초과인지 0도 이상인지
+
+# temp = int(input("온도: "))
+# print(temp > 80)
+# print(temp >= 0)
+
+# a = int(input("첫 번째 숫자: "))
+# b = int(input("두 번째 숫자: "))
+# c = int(input("세 번째 숫자: "))
+# avg = (a + b + c) / 3
+# print(avg > 80)
