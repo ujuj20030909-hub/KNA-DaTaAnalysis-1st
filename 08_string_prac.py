c3c29daa00a28cc0ad325af4cb913161f91fa0c3
ujuj20030909-hub <ujuj20030909@gmail.com>
2026-07-26 23:28:01 +0900

    study- 삼중따옴표, 이스케이프 문자

---
+++ 08_string_prac.py
@@ -0,0 +1,29 @@
+# 실습
+
+# code = "PUMP_A"
+# state = "정상"
+# hour = 1200
+# date = "2026-07-16"
+# card = "설비: " + code + "\n상태: " + state + "\n가동: " + str(hour) + "\n점검: " + date
+# print(card)
+
+# word = "temp_sensor"
+# print(word[:4])
+
+# word = "temp_sensor"
+# print(word[5:])
+
+# word = "sensor_01"
+# print(word[-2:])
+
+# word = "PYTHON"
+# print(word[::2])
+
+# word = "PYTHON"
+# print(word[::-1])
+
+# ph = "01012345678"
+# print(len(ph))
+
+# print("a,b,c,d".count(",")) # 3
+# print("a,b,c,d".count(", ")) #0
