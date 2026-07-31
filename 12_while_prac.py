1ba000bbe1cbfc5c5783ddf8a7d1b8ac739ad03c
ujuj20030909-hub <ujuj20030909@gmail.com>
2026-07-31 09:07:57 +0900

    study- while 실습

---
+++ 12_while_prac.py
@@ -0,0 +1,62 @@
+# 실습
+# answer = 7
+# s = 0
+# while s != answer:
+#     s = int(input("값을 입력하세요: "))
+# print("정답입니다.")
+
+# up down 게임
+# 1~50 중 하나의 숫자를 정답으로 저장
+# 사용자의 입력값 기준으로 정답이 up인지 down인지 출력하기
+# 정답이 나오면"정답입니다", 게임이 종료되었다고 출력
+
+answer = 29
+s = 0
+
+# while s != answer:
+#     s = int(input("숫자 입력: "))
+
+#     if s > answer:
+#         print("down")
+#     elif s < answer:
+#         print("up")
+#     else:
+#         print("정답입니다!")
+#         break
+
+# print("게임 종료")
+
+
+# total = 0
+
+# for i in [4, 7, 6]:
+#     if i > 5:
+#         fotla += i
+# print("합계: ", total)
+
+# n = int(input("횟수: "))
+# f = False
+# for i in range(n):
+#     m = int(input("측정값: "))
+
+#     if m > 80:
+#         f = True
+#         break
+# if f:
+#     print("발견")
+# else:
+#     print("없음")
+
+# temps = [22, 23, 28, 31, 33]
+# for t in temps:
+#     if t > 30:
+#         print("고온: ", t)
+
+# temps = [22, 37, 26, 25, 33, 23, 39, 40]
+# total = 0
+# c = 0
+# for t in temps:
+#     if t > 30:
+#         total += t
+#         c += 1
+# print("고온 평균: ", total / c)
